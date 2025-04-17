c2e1f2238223224f362e1a593eb841e17d19ecd6
liuyuhan1997 <36769381+liuyuhan1997@users.noreply.github.com>
2025-04-17 01:16:49 -0400

    rename

--- ios/OpenCourierTests/OpenCourierTests.m
+++ ios/OpenCourierTests/OpenCourierTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface OpenDeliTests : XCTestCase
+@interface OpenCourierTests : XCTestCase
 
 @end
 
-@implementation OpenDeliTests
+@implementation OpenCourierTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
